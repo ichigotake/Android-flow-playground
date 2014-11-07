47ce0037778a48f7c595b3411b1f0543cb0abb41
Takayuki Otake <otake@studyplus.jp>
2014-11-07 12:24:53 +0900

    Create project

---
+++ app/src/main/java/net/ichigotake/playground/screenstructure/MainActivity.java
@@ -0,0 +1,16 @@
+package net.ichigotake.playground.screenstructure;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.activity_main_toolbar);
+		setSupportActionBar(toolbar);
+	}
+}
